35b80a73ef86865a717b90aba9d6976fd2bbc65c
rishikareddy8 <rishikakothakapu8@gmail.com>
2026-03-19 13:13:44 +0530

    add login route

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,18 @@ const router= express.Router()
 const bcrypt=require('bcrypt')
 const jwt= require('jsonwebtoken')
 const User= require('../models/User')
-const {z}=require('zod')
+const {z, success}=require('zod')
 
 const signupSchema=z.object({
     name:z.string(),
     email:z.string().email(),
     password:z.string().min(6),
     role:z.enum(['teacher','student'])
+})
+
+const loginSchema=z.object({
+    email: z.string().email(),
+    password:z.string().min(6)
 })
 
 router.post('/signup', async(req,res)=>{
@@ -25,6 +30,29 @@ router.post('/signup', async(req,res)=>{
     const hashedPassword= await bcrypt.hash(password,10)
     const user= await User.create({name, email, password:hashedPassword, role})
     res.status(201).json({success:true, data:{_id:user._id, name: user.name, email:user.email, role:user.role}})
+})
+
+router.post('/login', async(req,res)=>{
+    const result= loginSchema.safeParse(req.body)
+    if(!result.success){
+        return res.status(400).json({success:false, error:'invalid request schema'})
+    }
+    const {email, password}=result.data
+    const existinguser=await User.findOne({email})
+    if(!existinguser){
+        return res.status(400).json({success:false, error:'invalid email or password'})
+    }
+
+    const validpassword= await bcrypt.compare(password, existinguser.password)
+    if(!validpassword){
+        return res.status(400).json({success:false, error:'invalid email or password'})
+    }
+
+    const token=jwt.sign(
+        {userId: existinguser._id, role: existinguser.role},
+        process.env.JWT_SECRET
+    )
+    return res.status(200).json({success:true, data:{token}})
 })
 
 
