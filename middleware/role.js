20a41501f83d8e930cc6e230a8da85e22a9e8257
rishikareddy8 <rishikakothakapu8@gmail.com>
2026-03-21 15:14:19 +0530

    add role middleware

---
+++ middleware/role.js
@@ -0,0 +1,10 @@
+const roleMiddleware=(role)=>{
+    return (req,res,next)=>{
+        if(req.user.role!==role){
+            return res.status(403).json({success:false, error:'forbidden, teacher access required'})
+        }
+        next()
+    }
+}
+
+module.exports= roleMiddleware
